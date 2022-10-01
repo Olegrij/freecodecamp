dd23e42b50e97f2990cc9eaf5de80b25c711dc64
Rijko <olegrij90@gmail.com>
2022-10-01 18:20:34 +0300

    Objects step

---
+++ Freecodecamp/Object.js
@@ -0,0 +1,16 @@
+let user = {
+	name: "John",
+	surname: "Smith",
+};
+
+user.name = "Pete";
+delete user.name;
+
+let
+	salaries = {
+		John: 100,
+		Ann: 160,
+		Pete: 130,
+	}
+
+alert(salaries.Ann + salaries.John + salaries.Pete);
