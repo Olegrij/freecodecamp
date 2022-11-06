4adbef8bb0bad311065eb355226eb583109f9952
Rijko <olegrij90@gmail.com>
2022-11-06 22:03:03 +0200

    new

--- Freecodecamp/JS.js
+++ Freecodecamp/JS.js
@@ -1,81 +1,10 @@
-/*const objectA = {
-	a: 10,
-	b: true
-}
-
-const copyOfA = objectA
-
-copyOfA.a = 20
-console.log(objectA.a)
-
-copyOfA.c = 'abc'
-
-console.log(objectA)
-*/
-
-/*
-let a = 'abc'
-
-console.log(typeof a);
-
-
-a = 20
-
-console.log(a);
-
-*/
-
-/*
-const myCity = {
-	city: 'New York'
-}
-
-myCity['popular'] = true
-
-console.log(myCity)
-
-const countryPropertyName = 'country'
-
-myCity[countryPropertyName] = 'USA'
-
-console.log(myCity)
-
-const myNewCity = {
-	city: 'New York',
-	info: {
-		isPopular: true,
-		country: 'USA'
-	}
-}
-
-console.log(myNewCity.info.isPopular)
-
 const name = 'Oleg'
 const postQty = 23
 
 const userProfile = {
-	name: name,
-	postQty: postQty,
+	name,
+	postQty,
 	hasSignedAgreement: false
 }
 
 console.log(userProfile)
-
-const userSecProfile = {
-	name,
-	postQty,
-	vasek: 5
-}
-
-console.log(userSecProfile)
-
-
-const a = 10
-
-*/
-
-const myName = "oleg";
-
-console.log(myName)
-
-myName = 10;
